2bcb9f684534300443ff4544b8d4cc95f37d664d
petehumphries <47575321+petehumphries@users.noreply.github.com>
2019-07-19 14:53:21 +0100

    Add files via upload

---
+++ Python_in_a_Nutshell/f_CRR1979_fft.py
@@ -0,0 +1,42 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#
+# Valuation of European Call Option in CRR1979 Model
+# Fast Fourier Transformation
+# FFT Version
+# C:\Users\User1\f_CRR1979_fft
+#
+
+import numpy as np
+from numpy.fft import fft, ifft
+from c_parameters import *
+
+# Array Generation for Index Levels
+md = np.arange(M + 1)
+mu = np.resize(md[-1], M + 1)
+mu = u ** (mu - md)
+md = d ** md
+S = S0 * mu * md
+
+# Valuation by FFT
+C_T = np.maximum(S - K, 0)
+Q = np.zeros(M + 1, 'd')
+Q[0] = q
+Q[1] = 1 - q
+l = np.sqrt(M + 1)
+v1 = ifft(C_T) * l
+v2 = (np.sqrt(M + 1) * fft(Q) / (l * (1 + r * dt))) ** M
+C_0 = fft(v1 * v2) / l
+
+# Result Output
+print("Value of European call option is %8.3f" % np.real(C_0[0]))
+
+#path sanity check
+import os
+print(os.path.abspath("f_CRR1979_fft"))
+print(os.path.abspath("c_parameters"))
+
